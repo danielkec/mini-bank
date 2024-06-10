a1168d134ece2eb2fff1272b50f45e8cd25debf9
Daniel Kec <daniel.kec@oracle.com>
2024-06-10 15:45:42 +0200

    Update

--- transaction-service/src/main/java/io/helidon/example/lra/booking/TransactionResource.java
+++ transaction-service/src/main/java/io/helidon/example/lra/booking/TransactionResource.java
@@ -9,8 +9,10 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.HeaderParam;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -26,9 +28,15 @@ public class TransactionResource {
 
     private static final System.Logger LOG = System.getLogger(TransactionResource.class.getName());
 
+    private final WebTarget target;
+
     @Inject
-    @ConfigProperty(name = "account.service.url")
-    private URI accountServiceUrl;
+    public TransactionResource(@ConfigProperty(name = "account.service.url")
+                               URI accountServiceUrl) {
+        this.target = ClientBuilder
+                .newClient()
+                .target(accountServiceUrl);
+    }
 
     @PUT
     @Path("/transfer")
@@ -36,29 +44,26 @@ public class TransactionResource {
     public Response transfer(@HeaderParam(LRA.LRA_HTTP_CONTEXT_HEADER) Optional<URI> lraId,
                              TransferOperation operation) {
         var withdrawOperation = new AccountOperation(operation.from(), operation.amount());
-        try (var c = ClientBuilder.newClient()) {
-            var target = c.target(accountServiceUrl);
-            try (var res = target
-                    .path("/account/withdraw")
-                    .request()
-                    .put(Entity.entity(withdrawOperation, MediaType.APPLICATION_JSON_TYPE))) {
-                if (res.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
-                    return Response.status(res.getStatusInfo().getStatusCode())
-                            .tag("Money withdrawal failed.")
-                            .build();
-                }
+        try (var res = target
+                .path("/account/withdraw")
+                .request()
+                .put(Entity.entity(withdrawOperation, MediaType.APPLICATION_JSON_TYPE))) {
+            if (res.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
+                return Response.status(res.getStatusInfo().getStatusCode())
+                        .tag("Money withdrawal failed.")
+                        .build();
             }
+        }
 
-            var enterOperation = new AccountOperation(operation.to(), operation.amount());
-            try (var res = target
-                    .path("/account/enter")
-                    .request()
-                    .put(Entity.entity(enterOperation, MediaType.APPLICATION_JSON_TYPE))) {
-                if (res.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
-                    return Response.status(res.getStatusInfo().getStatusCode())
-                            .tag("Money enter failed.")
-                            .build();
-                }
+        var enterOperation = new AccountOperation(operation.to(), operation.amount());
+        try (var res = target
+                .path("/account/enter")
+                .request()
+                .put(Entity.entity(enterOperation, MediaType.APPLICATION_JSON_TYPE))) {
+            if (res.getStatusInfo().getFamily() != Response.Status.Family.SUCCESSFUL) {
+                throw new WebApplicationException(Response.status(res.getStatusInfo().getStatusCode())
+                        .tag("Money enter failed.")
+                        .build());
             }
         }
 
